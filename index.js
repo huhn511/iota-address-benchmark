f44f2a3309ab8e204aee51cda24dd5bde0b66a04
huhn511 <sehe89@gmail.com>
2020-05-29 20:48:29 +0200

    add address generation with iota.js

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 
 console.log("index.js called.")
 
-async function run() {
+async function runWasm() {
+    console.log("run wasm")
     let wasmmodule = await import('./pkg')
 
-    console.log("wasmmodule")
     console.log(wasmmodule)
     
-    let addresses = wasmmodule.generate_addresses('9'.repeat(81), 0, 9, 1, true)
-    
-    console.log("addresses")
-    console.log(addresses)
+    var t0 = performance.now()
+    let address = wasmmodule.generate_address('9'.repeat(81), 0, 1, true)
+    var t1 = performance.now()
+
+    console.log("wasm generated address: ", address)
+    console.log("Time for WASM generating address " + (t1 - t0) + " milliseconds.")
+
 };
 
-run()
+runWasm()
+
+import { generateAddress } from '@iota/core'
+async function runNormal() {
+    console.log("run normal js")
+    
+    var t0 = performance.now()
+    let address = generateAddress('9'.repeat(81), 0, 1, true)
+    var t1 = performance.now()
+
+    console.log("iota.js generated address: ", address)
+    console.log("Time for normal generating address for iota.js " + (t1 - t0) + " milliseconds.")
+
+};
+
+runNormal()
